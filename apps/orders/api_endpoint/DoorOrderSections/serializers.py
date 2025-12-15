75651c2861602087dd0489773b7974a1e131ad8f
Zarnigor <zarnigor1008@gmail.com>
2025-12-15 11:02:51 +0500

    Fixed: some minor bugs

--- apps/orders/api_endpoint/DoorOrderSections/serializers.py
+++ apps/orders/api_endpoint/DoorOrderSections/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 
-from apps.orders.models import WindowOrderSection
+from apps.orders.models import DoorOrderSection
 
 
-class WindowOrderSectionSerializer(serializers.ModelSerializer):
+class DoorOrderSectionSerializer(serializers.ModelSerializer):
     area_m2 = serializers.FloatField(read_only=True)
     orientation = serializers.CharField(source="template_section.orientation", read_only=True)
     section_type = serializers.CharField(source="template_section.section_type", read_only=True)
     has_glass = serializers.BooleanField(source="template_section.has_glass", read_only=True)
 
     class Meta:
-        model = WindowOrderSection
+        model = DoorOrderSection
         fields = (
             "section_order",
             "template_section",
